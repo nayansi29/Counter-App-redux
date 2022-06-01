74efd9d54b8dce0935ecbcbdb196c8ab380d393b
nancy <nancypurwar5@gmail.com>
2022-06-01 22:11:42 +0530

    first commit

---
+++ src/components/Counter.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { increment, decrement } from '../actions';
+
+const Counter = (props) => {
+    return (
+        <div>
+            <button onClick={props.increment} >increment</button>
+            <span>{props.count}</span>
+            <button onClick={props.decrement}>decrement</button>
+        </div>
+    )
+};
+const mapStateToProps = (state) => {
+    return { count: state.count };
+}
+
+export default connect(mapStateToProps, { increment, decrement })(Counter);
---
+++ src/reducer/index.js
@@ -0,0 +1,18 @@
+import { combineReducers } from "redux";
+
+
+const countReducer = (count = 0, action) => {
+    if (action.type === 'increment') {
+        return count + 1;
+    }
+    else if (action.type === 'decrement') {
+        return count - 1;
+    }
+    else {
+        return count;
+    };
+};
+
+export default combineReducers({
+    count: countReducer,
+});
